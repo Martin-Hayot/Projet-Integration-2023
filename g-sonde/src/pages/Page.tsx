37dec519b10048b3423527b7ae5ef53697cac306
JarodEphec <j.boulangerflemal@students.ephec.be>
2023-10-04 12:47:50 +0200

    [ADD] Simple comunication between front & back end

--- g-sonde/src/pages/Page.tsx
+++ g-sonde/src/pages/Page.tsx
@@ -3,9 +3,21 @@ import { useParams } from 'react-router';
 import ExploreContainer from '../components/ExploreContainer';
 import './Page.css';
 
+import { useEffect, useState } from "react";
+import axios from "axios";
+
 const Page: React.FC = () => {
 
   const { name } = useParams<{ name: string; }>();
+
+  const [data, setData] = useState();
+  useEffect(() => {
+    axios.get('http://51.68.172.36:3000/getDataAquarium')
+      .then((response) => {
+        setData(response.data);
+        console.log(data);
+      })
+  }, []);
 
   return (
     <IonPage>
@@ -19,6 +31,16 @@ const Page: React.FC = () => {
       </IonHeader>
 
       <IonContent fullscreen>
+        <IonContent>
+          {data && (
+            <ul>
+              {(data as Array<{ _id: number, data: number }>).map(item => (
+                <li key={item._id}>{item.data}</li>
+              ))}
+            </ul>
+          )}
+
+        </IonContent>
         <IonHeader collapse="condense">
           <IonToolbar>
             <IonTitle size="large">{name}</IonTitle>
