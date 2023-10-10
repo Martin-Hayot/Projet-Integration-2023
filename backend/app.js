324c7fa17013e8b750ab7704ef5053b41ad029ed
Martin-Hayot <116266177+Martin-Hayot@users.noreply.github.com>
2023-10-10 17:29:41 +0200

    Update app.js route User mal nommé

--- backend/app.js
+++ backend/app.js
@@ -19,7 +19,7 @@ mongoose
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cors());
-app.use("/api", PersonRoute);
+app.use("/api/user", PersonRoute);
 
 app.post('/aquarium/data', (req, res, next) => {
     const aquarium = new Aquarium({
