39b8bf8e07dd240e157b12b136012a9129bdd57b
basil-wecodx <basil@wecodx.com>
2023-10-26 10:47:26 +0200

    fix: correction du problème de taille de la balise de la sonde

--- g-sonde/src/pages/Home.tsx
+++ g-sonde/src/pages/Home.tsx
@@ -56,10 +56,10 @@ const Home: React.FC = () => {
 					</div>
 
 					<section className="bg-aquarium dark:bg-aquarium mx-auto text-center h-screen bg-cover bg-center bg-no-repeat relative">
-						<h1 className="text-5xl pt-48 mb-6 text-white">Desktop Home</h1>
+						<h1 className="text-5xl pt-[8%] mb-6 text-white">Desktop Home</h1>
 						<p className="mb-12 text-white">This is desktop-specific content.</p>
 						<div className="absolute inset-0 bg-center dark:bg-black"></div>
-						<div className="group relative m-0 flex h-72 w-96 rounded-xl shadow-xl ring-gray-900/5 sm:mx-auto sm:max-w-lg" onClick={() => history.push("/login")}>
+						<div className="group relative m-0 flex w-full h-full sm:w-48 sm:h-36 md:w-72 md:h-56 lg:w-96 lg:h-72 max-w-96 max-h-72 rounded-xl shadow-xl ring-gray-900/5 sm:mx-auto sm:max-w-lg" onClick={() => history.push("/login")}>
 							<div className="z-10 h-full w-full overflow-hidden rounded-xl border border-gray-200 opacity-80 transition duration-300 ease-in-out group-hover:opacity-100 dark:border-gray-700 dark:opacity-70">
 							<img src="sonde.png" className="animate-fade-in block h-full w-full scale-100 transform object-cover object-center opacity-100 transition duration-300 group-hover:scale-110" alt="" />
 							</div>
@@ -69,7 +69,7 @@ const Home: React.FC = () => {
 						</div>
 						
 					</section>
-					<footer className="bg-transparent fixed bottom-0 w-full sm:h-12 md:h-16 lg:h-20 xl:h-24 backdrop-blur"> 
+					<footer className="bg-transparent fixed bottom-0 w-full sm:h-12 md:h-16 lg:h-20 xl:h-24 backdrop-blur-sm"> 
 						<div className="footer-content flex justify-center items-center mt-4 text-xl font-serif">
     						G-Sonde, developed thanks to Valérie Ramakers
   						</div>
