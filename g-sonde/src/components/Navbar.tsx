4b069ea21d2f98969122decb6477fabb07cb0f64
Martin-Hayot <m.hayot@students.ephec.be>
2023-11-22 17:04:16 +0100

    refactor: navbar pour mobile et responsive

--- g-sonde/src/components/Navbar.tsx
+++ g-sonde/src/components/Navbar.tsx
@@ -1,35 +1,98 @@
-import { Link } from 'react-router-dom';
-import { IonButton } from '@ionic/react';
-const Navbar: React.FC = () => {
-	return (
-		<div className='flex justify-between'>
-			<div className='text-3xl m-6'>
-				<Link to=''>G-Sonde</Link>
-			</div>
-			<nav className='flex justify-end items-center text-2xl m-6'>
-				<ul className='flex gap-12'>
-					<li className=''>
-						<Link to='/'>Home</Link>
-					</li>
-					<li>
-						<Link to='/about'>About</Link>
-					</li>
-					<li>
-						<Link to='/contact'>Contact</Link>
-					</li>
-				</ul>
+import { Link } from "react-router-dom";
+import { IonButton } from "@ionic/react";
+import React, { useEffect, useRef } from "react";
 
-				<div className='mx-12'>
-					<IonButton className='mr-8' routerLink='/login'>
-						Log in
-					</IonButton>
-					<IonButton className='' routerLink='/signup'>
-						Sign up
-					</IonButton>
-				</div>
-			</nav>
-		</div>
-	);
+import "../../styles/navbar.css";
+
+const Navbar: React.FC = () => {
+    const menu: any = useRef(null);
+    const menuBtn: any = useRef(null);
+    const toggleMenu = () => {
+        menu.current.classList.toggle("hidden");
+        menu.current.classList.toggle("flex");
+        menuBtn.current.classList.toggle("open");
+    };
+
+    useEffect(() => {
+        menu.current = document.getElementById("menu");
+        menuBtn.current = document.getElementById("menu-btn");
+        menuBtn.current.addEventListener("click", toggleMenu);
+        const popstateHandler = () => {
+            menu.current.classList.toggle("hidden");
+            menu.current.classList.toggle("flex");
+            menuBtn.current.classList.toggle("open");
+        };
+        window.addEventListener("popstate", popstateHandler);
+
+        return () => {
+            menuBtn.current.removeEventListener("click", toggleMenu);
+            window.removeEventListener("popstate", popstateHandler);
+        };
+    }, []);
+
+    return (
+        <div>
+            <nav className="flex items-center text-2xl justify-between text-neutral-200 h-20 mx-10">
+                <a
+                    href="/"
+                    className="hover:scale-110 z-30 text-3xl transition-all duration-200 hover:text-white"
+                >
+                    G-Sonde
+                </a>
+                <div className="hidden lg:flex h-10 space-x-10 lg:space-x-12 xl:space-x-16 lg:pr-6 xl:pr-12">
+                    <a
+                        href="/about"
+                        className="hover:scale-110 transition-all duration-200 hover:text-white"
+                    >
+                        About
+                    </a>
+                    <a
+                        href="/contact"
+                        className="hover:scale-110 transition-all duration-200 hover:text-white"
+                    >
+                        Contact
+                    </a>
+                    <div className="flex flex-row gap-6 items-center">
+                        <IonButton href="/login" className="">
+                            Login
+                        </IonButton>
+                        <IonButton href="/register">Register</IonButton>
+                    </div>
+                </div>
+                <div className="lg:hidden mt-2">
+                    <button
+                        className="z-40 block hamburger lg:hidden focus:outline-none"
+                        id="menu-btn"
+                        type="button"
+                    >
+                        <span className="hamburger-top"></span>
+                        <span className="hamburger-middle"></span>
+                        <span className="hamburger-bottom"></span>
+                    </button>
+                </div>
+            </nav>
+            <div
+                id="menu"
+                className="absolute z-20 hidden top-0 bottom-0 left-0 flex-col self-end w-full min-h-screen py-1 pt-40 pl-12 space-y-3 text-xl text-white uppercase bg-black"
+            >
+                <a href="/" className="hover:text-red-500">
+                    Home
+                </a>
+                <a href="/about" className="hover:text-red-500">
+                    About
+                </a>
+                <a href="/contact" className="hover:text-red-500">
+                    Contact
+                </a>
+                <a href="/login" className="hover:text-red-500">
+                    Login
+                </a>
+                <a href="/signup" className="hover:text-red-500">
+                    Register
+                </a>
+            </div>
+        </div>
+    );
 };
 
 export default Navbar;
