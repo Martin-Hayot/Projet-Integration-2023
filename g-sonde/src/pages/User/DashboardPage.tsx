378b17f768ab2bd453d1fa5e60a680dc0f336ebc
Martin-Hayot <m.hayot@students.ephec.be>
2023-11-30 12:31:45 +0100

    US14bis feat: protection des routes sur le client
    Présentation de la pull request
    Simple fonction réutilisable permettant de protéger un composant ou une page.
    
    Explication
    Pour protéger il faut utiliser le hook useHistory et useEffect ensuite importer le composant requireAuth dans la page ou composant à protéger.
    
    voici comment faire :
    
    import { useEffect } from "react";
    import { useHistory } from "react-router-dom";
    
    const history = useHistory();
    
    useEffect(() => {
        requireAuth(history);
    });

--- g-sonde/src/pages/User/DashboardPage.tsx
+++ g-sonde/src/pages/User/DashboardPage.tsx
@@ -1,79 +1,86 @@
 import {
-	IonButton,
-	IonButtons,
-	IonContent,
-	IonFooter,
-	IonHeader,
-	IonMenu,
-	IonMenuButton,
-	IonPage,
-	IonSplitPane,
-	IonTitle,
-	IonToolbar,
-	isPlatform,
-} from '@ionic/react';
-import { Link } from 'react-router-dom';
-import axios from 'axios';
-import React from 'react';
-import { UserContext } from '../../components';
+    IonButton,
+    IonButtons,
+    IonContent,
+    IonFooter,
+    IonHeader,
+    IonMenu,
+    IonMenuButton,
+    IonPage,
+    IonSplitPane,
+    IonTitle,
+    IonToolbar,
+    isPlatform,
+} from "@ionic/react";
+import { Link, useHistory } from "react-router-dom";
+import axios from "axios";
+import React, { useEffect } from "react";
+import { UserContext } from "../../components";
+import requireAuth from "../../utils/requireAuth";
 
 const DashboardPage: React.FC = () => {
-	const { logout } = React.useContext(UserContext);
-	async function localLogout() {
-		logout();
-		window.location.href = '/';
-	}
-	const [data, setData] = React.useState();
+    const { logout } = React.useContext(UserContext);
+    async function localLogout() {
+        logout();
+        window.location.href = "/";
+    }
+    const [data, setData] = React.useState();
 
-	async function getData() {
-		axios
-			.get('http://localhost:3001/api/user', {
-				withCredentials: true,
-			})
-			.then((res) => {
-				setData(res.data.message);
-			})
-			.catch((err) => {
-				window.location.href = '/login';
-			});
-	}
+    const history = useHistory();
 
-	return (
-		<>
-			<IonMenu contentId='main-content'>
-				<IonHeader>
-					<IonToolbar>
-						<IonTitle>Menu Content</IonTitle>
-					</IonToolbar>
-				</IonHeader>
-				<IonContent className='ion-padding'>
-					This is the menu content.
-					<div>
-						<Link to={'/contact'}>Contact</Link>
-					</div>
-					<IonButton expand='block' onClick={localLogout}>
-						Logout
-					</IonButton>
-				</IonContent>
-			</IonMenu>
-			<IonPage id='main-content'>
-				<IonHeader>
-					<IonToolbar>
-						<IonButtons slot='start'>
-							<IonMenuButton></IonMenuButton>
-						</IonButtons>
-						<IonTitle>Menu</IonTitle>
-					</IonToolbar>
-				</IonHeader>
-				<IonContent className='ion-padding'>
-					Tap the button in the toolbar to open the menu.
-					<br />
-					<IonButton onClick={getData}>Get Data</IonButton>
-					<div>{data}</div>
-				</IonContent>
-			</IonPage>
-		</>
-	);
+    useEffect(() => {
+        requireAuth(history);
+    });
+
+    async function getData() {
+        axios
+            .get("http://localhost:3001/api/user", {
+                withCredentials: true,
+            })
+            .then((res) => {
+                setData(res.data.message);
+            })
+            .catch((err) => {
+                window.location.href = "/login";
+            });
+    }
+
+    return (
+        <>
+            <IonMenu contentId="main-content">
+                <IonHeader>
+                    <IonToolbar>
+                        <IonTitle>Menu Content</IonTitle>
+                    </IonToolbar>
+                </IonHeader>
+                <IonContent className="ion-padding">
+                    This is the menu content.
+                    <div>
+                        <Link to={"/contact"}>Contact</Link>
+                    </div>
+                    <IonButton expand="block" onClick={localLogout}>
+                        Logout
+                    </IonButton>
+                </IonContent>
+            </IonMenu>
+            <IonPage id="main-content">
+                <IonHeader>
+                    <IonToolbar>
+                        <IonButtons slot="start">
+                            <IonMenuButton></IonMenuButton>
+                        </IonButtons>
+                        <IonTitle>Menu</IonTitle>
+                    </IonToolbar>
+                </IonHeader>
+                <IonContent className="ion-padding">
+                    Tap the button in the toolbar to open the menu.
+                    <br />
+                    <IonButton onClick={getData}>Get Data</IonButton>
+                    <div>{data}</div>
+                </IonContent>
+            </IonPage>
+        </>
+    );
 };
 
 export default DashboardPage;
