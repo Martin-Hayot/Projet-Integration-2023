06d0aa32920e34751766ba21299da015c8c23ea7
Martin-Hayot <m.hayot@students.ephec.be>
2023-12-06 18:43:56 +0100

    US16 feat: page de profile Description de la US Cet US consiste à créer une page de profil qui permet de changer une photo de profil, et ses informations personnelles tout en redemandant une authentification pour sécuriser l'utilisateur.
    lien us : https://github.com/users/Martin-Hayot/projects/2?pane=issue&itemId=42738463
    
    Modification
    Frontend
    Nécessite l'authentification pour pouvoir y accéder
    Une page contenant la possibilité de changer sa photo de profil et un formulaire pour les informations personnelles.
    Un popup s'affiche lorsqu'on clique sur apply changes (appliquer les modifs) qui nous demandent nos informations de connexion.
    Lorsque l'utilisateur est authentifié, affiche un Toast en fonction de si tout c'est bien passé ou s'il y a eu une erreur.
    Backend
    Ajout d'une route user contenant 3 nouveau endpoint
    put "/profile/picture"
    get "/profile/picture"
    patch "/profile"
    Nouvelle entrée dans la db : profilePicture

--- backend/routes/User.js
+++ backend/routes/User.js
@@ -2,32 +2,96 @@ const express = require("express");
 const { requireUser } = require("../middleware/auth");
 const User = require("../models/user");
 const router = express.Router();
+const bcrypt = require("bcrypt");
 
-router.get("/", requireUser, async (req, res) => {
-	const searchedUser = await User.findOne({ email: req.user.email }).select(
-		"password"
-	);
-	res
-		.status(200)
-		.json({ message: "Hello from the server side!", user: searchedUser });
+router.get("/profile", requireUser, async (req, res) => {
+    try {
+        const searchedUser = await User.findOne({
+            email: req.user.email,
+        }).select("firstname lastname email");
+        if (!searchedUser) {
+            return res.status(404).json({
+                message: "User not found!",
+            });
+        }
+        res.status(200).json(searchedUser);
+    } catch (error) {
+        res.status(500).json({
+            message: "Something went wrong! " + error.message,
+        });
+    }
 });
 
-router.get("/ability", requireUser, async (req, res) => {
-	const searchedUser = await User.findOne({ email: req.user.email }).select(
-		"ability"
-	);
-	return res.status(200).json({
-		message: "Ability sent successfully",
-		ability: searchedUser.ability,
-	});
+router.get("/profile/picture", requireUser, async (req, res) => {
+    const { userId } = req.user;
+    const getProfilePicture = await User.findById(userId).select(
+        "profilePicture"
+    );
+    res.status(200).json({ profilePicture: getProfilePicture.profilePicture });
 });
 
-router.get("/:id", requireUser, async (req, res) => {
-	const searchedUser = await User.findOne({ email: req.user.email });
-	return res.status(200).json({
-		message: "User sent successfully",
-		user: searchedUser,
-	});
+router.patch("/profile", requireUser, async (req, res) => {
+    const { userId } = req.user;
+    const { firstname, lastname, email, password } = req.body;
+    if (!firstname || !lastname || !email || !password) {
+        return res.status(400).json({
+            message: "All entries are required!",
+        });
+    }
+    try {
+        const getUserPassword = await User.findById(userId).select("password");
+        if (await bcrypt.compare(password, getUserPassword.password)) {
+            return res.status(400).json({
+                message: "New password cannot be the same as old password!",
+            });
+        }
+        const hashedPassword = await bcrypt.hash(password, 10);
+        const updateUser = await User.findByIdAndUpdate(userId, {
+            firstname,
+            lastname,
+            email,
+            password: hashedPassword,
+        });
+        if (!updateUser) {
+            return res.status(404).json({
+                message: "User not found!",
+            });
+        }
+        res.status(200).json({
+            message: "User updated successfully!",
+        });
+    } catch (error) {
+        res.status(500).json({
+            message: "Something went wrong! " + error.message,
+        });
+    }
+});
+
+router.put("/profile/picture", requireUser, async (req, res) => {
+    const { userId } = req.user;
+    const { profilePicture } = req.body;
+    if (!profilePicture) {
+        return res.status(400).json({
+            message: "Profile picture is required!",
+        });
+    }
+    try {
+        const updateProfilePicture = await User.findByIdAndUpdate(userId, {
+            profilePicture,
+        });
+        if (!updateProfilePicture) {
+            return res.status(404).json({
+                message: "User not found!",
+            });
+        }
+        res.status(200).json({
+            message: "Profile picture updated successfully!",
+        });
+    } catch (error) {
+        res.status(500).json({
+            message: "Something went wrong! " + error.message,
+        });
+    }
 });
 
 module.exports = router;
